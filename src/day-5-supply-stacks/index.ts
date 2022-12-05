9f06915589f8ee71b7661ff754e966e18998e391
Liam Wibberley <liam.wibbeley@cef.co.uk>
2022-12-05 09:42:20 +0000

    transformation of stacks

--- src/day-5-supply-stacks/index.ts
+++ src/day-5-supply-stacks/index.ts
@@ -1,8 +1,45 @@
 class DayFivePuzzle {
   constructor(private puzzleInput: string) {}
 
+  private getStacks(): string[][] {
+    const charArray = [...this.puzzleInput];
+
+    const letterRegex = /[A-Z]/g;
+    const newLineRegex = /\r?\n/;
+    let stackIndex = 0;
+
+    return charArray.reduce<string[][]>((previous, current) => {
+      if (current.match(newLineRegex)) {
+        stackIndex = 0;
+      }
+
+      if (current.match(letterRegex)) {
+        if (!previous[stackIndex]) {
+          previous[stackIndex] = [];
+        }
+
+        previous[stackIndex].push(current);
+
+        stackIndex++;
+      }
+
+      return previous;
+    }, []);
+  }
+
+  private getTopCrateValueFromStack(stack: string[]): string {
+    return stack[0];
+  }
+
   public calculateTaskOne(): string {
-    return '';
+    const stacks = this.getStacks();
+
+    console.log('stacks', stacks);
+
+    return stacks.reduce<string>(
+      (previous, current) => previous + this.getTopCrateValueFromStack(current),
+      ''
+    );
   }
 
   public calculateTaskTwo(): string {
